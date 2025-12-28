7cee8b2ad25e1662c388ef6899e2b59990bc6afa
Jeff <woolridgejeff@proton.me>
2025-12-28 14:25:40 -0330

    login2

--- createAdmin.js
+++ createAdmin.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
 import User from "./src/models/User.js";
 
-await mongoose.connect("mongodb://localhost:27017/chatapp");
+await mongoose.connect(
+  "ongodb+srv://jwool:ooguR2ku@chatapp.iu8ztkq.mongodb.net/chatapp?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const adminExists = await User.findOne({ username: "admin" });
 
 if (!adminExists) {
   const admin = new User({
     username: "admin",
-    password: "securepassword", 
-    isAdmin: true
+    password: "securepassword",
+    isAdmin: true,
   });
   await admin.save();
   console.log("Admin user created");
